4ed8cac7e2fe1c4cab342ee7b886a2e61a6dc8d2
rahuldevsutradhar <rahulkumar016332@gmail.com>
2025-06-11 09:11:33 +0600

    order component add home page

---
+++ src/components/OrderList.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+export default function RecentOrders() {
+  const orders = [
+    { id: "ORD-20250318-250", 
+      total: 3780, 
+      method: "Cash On Delivery" },
+    { id: "ORD-20250317-249", 
+      total: 1930, 
+      method: "Cash On Delivery" },
+    { id: "ORD-20250317-248", 
+      total: 1930, 
+      method: "Cash On Delivery" },
+  ];
+
+  return (
+    <div className="p-6 bg-white shadow-md rounded-2xl">
+      <div className="overflow-x-auto">
+        <table className="min-w-full border-collapse">
+          <thead>
+            <tr className="bg-white text-left">
+              <th className="px-6 py-3 font-medium">Customer</th>
+              <th className="px-6 py-3 font-medium">Order ID</th>
+              <th className="px-6 py-3 font-medium">Payment Method</th>
+              <th className="px-6 py-3 font-medium">Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {orders.map((order, index) => (
+              <tr
+                key={order.id}
+                className={index % 2 === 0 ? "bg-gray-200" : "bg-white"}
+              >
+                <td className="px-6 py-4"></td>
+                <td className="px-6 py-4 text-blue-600 font-open hover:underline cursor-pointer">
+                  {order.id}
+                </td>
+                <td className="px-6 py-4">{order.method}</td>
+                <td className="px-6 py-4">৳{order.total}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,14 @@ import Container from '../components/Container'
 import Navbar from '../layouts/Navbar'
 import Cart from '../components/Cart'
 import Flex from '../components/Flex'
+import Title from '../components/Title'
+import OrderList from '../components/OrderList'
 import CartTwo from '../components/CartTwo'
 import CartThree from '../components/CartThree'
 import CartFour from '../components/CartFour'
 import ChartCard from '../components/ChartCard'
 import { salesData, earningsData } from "../datas/chartData";
+import { GrOrderedList } from 'react-icons/gr'
 
 
 
@@ -18,6 +21,7 @@ const Home = () => {
         <Navbar />
         <Flex>
           <div className='w-2/12'></div>
+
           <div className='w-10/12 flex flex-col justify-center mx-8'>
             <div className=' w-auto bg-white shadow-sm my-5  '>
               <h1 className='text-[#575963] text-[17px] font-open px-3 py-4'>Dashboard</h1>
@@ -42,7 +46,7 @@ const Home = () => {
               <CartFour text='Total Order' number='0' />
             </div>
 
-            <div className="p-6 bg-white min-h-screen">
+            <div className="p-6 bg-white ">
               <div className="flex flex-col md:flex-row gap-6">
                 <ChartCard
                   title="Monthly Product Sales Report"
@@ -56,9 +60,15 @@ const Home = () => {
                 />
               </div>
             </div>
+            
+            <div className='mb-[50px]'>
+              <Title text='Recent Orders' className='text-[#575963] text-[17px] font-open px-3 py-4 shadow-md mb-4'/>
+            <OrderList/>
+            </div>
 
           </div>
         </Flex>
+          
 
       </Container>
     </section>
